ade83a80eec27244f809e36f125e1053b90d0615
Hanna1991-sys <119236984+Hanna1991-sys@users.noreply.github.com>
2024-01-04 18:15:23 +0200

    end

---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  padding: 12px;
+  background-color: turquoise;
+  border: 3px solid black;
+  border-radius: 4px;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+  color: black;
+  font-size: 20px;
+`;
+
+export const Input = styled.input`
+  padding: 8px 12px;
+  font: inherit;
+  cursor: pointer;
+`;
+
+export const Button = styled.button`
+  padding: 8px 12px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 3px solid black;
+  color: black;
+  font-size: 20px;
+`;
